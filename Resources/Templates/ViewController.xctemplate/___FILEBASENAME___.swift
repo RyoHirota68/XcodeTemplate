f53a41b911aeda4afde15e3ea0742bad0dcc576c
RyoHirota68 <ryo.0608.hiro@gmail.com>
2025-01-31 00:32:43 +0900

    update

--- Resources/Templates/ViewController.xctemplate/___FILEBASENAME___.swift
+++ Resources/Templates/ViewController.xctemplate/___FILEBASENAME___.swift
@@ -3,15 +3,15 @@
 import UIKit
 import SVProgressHUD
 
-final class ___VARIABLE_moduleName___ViewController: UIViewController {
+final class ___FILEBASENAME___ViewController: UIViewController {
     
-    private let presenter: ___VARIABLE_moduleName___Presenter!
+    private let presenter: ___FILEBASENAME___Presenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        presenter = ___VARIABLE_moduleName___Presenter(view: self,
-                                      service: ___VARIABLE_moduleName___Service())
+        presenter = ___FILEBASENAME___Presenter(view: self,
+                                                service: ___FILEBASENAME___Service())
         
         setupView()
         setupLayout()
